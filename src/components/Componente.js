c1e3660578ed5210b80b6d189edbe8b76a2857b1
sebastraits <sebasicario@gmail.com>
2021-04-12 21:54:43 -0300

    Inicio

---
+++ src/components/Componente.js
@@ -0,0 +1,15 @@
+import React, {Component} from "react";
+
+function Componente(props) {
+
+    return <h2>{props.msg}</h2>
+
+}
+
+// class Componente extends Component {
+// render() {
+//     return <h2>{this.props.msg}</h2>
+// }
+// }
+
+export default Componente
